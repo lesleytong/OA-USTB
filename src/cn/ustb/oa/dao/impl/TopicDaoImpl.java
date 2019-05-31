233bef8d27c6b0828138cfcd83bdd180a88b75d9
eclipse_user <eclipse_user@hello.com>
2019-05-31 09:53:08 +0800

    day6--9主题列表功能完

--- src/cn/ustb/oa/dao/impl/TopicDaoImpl.java
+++ src/cn/ustb/oa/dao/impl/TopicDaoImpl.java
@@ -24,8 +24,8 @@ public class TopicDaoImpl extends BaseDaoImpl<Topic> implements ITopicDao{
 	 */
 	@Override
 	public List<Topic> findTopicByForum(Forum model) {
-		
-		String hql = "FROM Topic t WHERE t.forum = ?";
+		//select * from ustb_topic order by (case type when 2 then 2 else 1 end) desc, postTime desc;
+		String hql = "FROM Topic t WHERE t.forum = ? ORDER BY (CASE t.type WHEN 2 THEN 2 ELSE 1 END) DESC, t.postTime DESC";
 		Query query = this.getSession().createQuery(hql);
 		query.setParameter(0, model);
 		return query.list();
